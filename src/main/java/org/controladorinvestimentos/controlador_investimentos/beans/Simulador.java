1e6088c0ce7bfb7b4fb8be173107d121b1c107d5
FilipedevGenz <filipe.asmoreira0704@gmail.com>
2025-02-22 00:40:03 -0300

    funcao de calculo da influencia percentual na carteira

--- src/main/java/org/controladorinvestimentos/controlador_investimentos/beans/Simulador.java
+++ src/main/java/org/controladorinvestimentos/controlador_investimentos/beans/Simulador.java
@@ -13,4 +13,30 @@ public class Simulador {
         Double valorAtual = APIrequest.buscarPrecoAtivoEmTempoReal(ativo);
         return valorMedioCompra - valorAtual;
     }
+
+    public static Double influenciaNaCarteira(Carteira carteira, String ativo) throws IOException {
+        
+        Double quantidade = carteira.repositorioRelatorio.getQuantidadeAtivo(ativo);
+
+        if (quantidade == 0) {
+            return 0.0;
+        }
+
+        Double variacaoPreco = simularVariacao(carteira, ativo);
+
+        Double impactoFinanceiro = quantidade * variacaoPreco;
+
+        Double valorTotalCarteira = carteira.repositorioRelatorio.valorDeCompraCarteira();
+
+        if (valorTotalCarteira == 0) {
+            return 0.0;
+        }
+
+        Double impactoPercentual = (impactoFinanceiro / valorTotalCarteira) * 100;
+
+        return impactoPercentual;
+    }
+
+
+
 }
